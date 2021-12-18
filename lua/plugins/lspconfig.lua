c0ddd93b3956f4a4408a9e41a6e1b7008e5638bc
Eduardo Rizzo <eduardorizzo1@outlook.com>
2021-12-18 16:34:10 -0300

    add typescript for lspconfig

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,1 +1,8 @@
-require'lspconfig'.pyright.setup{}
+local nvim_lsp = require('lspconfig')
+local protocol = require'vim.lsp.protocol'
+
+nvim_lsp.tsserver.setup {
+  on_attach = on_attach,
+  filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+  capabilities = capabilities
+}
