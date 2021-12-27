c545d0bd9e499bc05969c0c7a134c17dac198a6e
Eduardo Rizzo <eduardorizzo1@outlook.com>
2021-12-27 00:54:20 -0300

    add new bufferline config

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,20 +1,52 @@
 require('bufferline').setup{
-  options = {
+options = {
+    view = "multiwindow",
     separator_style = 'thin',
-    show_tab_indicator = true,
+    buffer_close_icon = "",
+    modified_icon = "",
+    show_close_icon = true,
+    close_icon = "",
+    show_buffer_close_icons = true,
+    left_trunc_marker = "",
+    right_trunc_marker = "",
+    max_name_length = 14,
     max_prefix_length = 15,
-    view = 'default',
+    tab_size = 20,
+    enforce_regular_tabs = false,
+    always_show_bufferline = true,
+    show_tab_indicators = true,
     diagnostic = 'nvim_lsp',
-    diagnostics_indicator = function(count, level, diagnostics_dict)
+    
+		diagnostics_indicator = function(count, level, diagnostics_dict)
       return "("..count..")"
     end,
-    offsets = {
+    
+		offsets = {
       { 
-	filetype = "NvimTree", 
-	text = '◀️ 🔍 💀 🧛 👻 🥷 👀 🔎 ▶️',
-	text_align = "center",
-	highlight = "Directory"
+				filetype = "NvimTree", 
+				text = '◀️ 🔍 💀 🧛 👻 🥷 👀 🔎 ▶️',
+				text_align = "center",
+				highlight = "Directory"
       }
-    }
+    },
+    
+		custom_filter = function(buf_number)
+       -- Func to filter out our managed/persistent split terms
+       local present_type, type = pcall(function()
+          return vim.api.nvim_buf_get_var(buf_number, "term_type")
+       end)
+
+       if present_type then
+          if type == "vert" then
+             return false
+          elseif type == "hori" then
+             return false
+          end
+          return true
+       end
+
+       return true
+    end,
   }
+
 }
